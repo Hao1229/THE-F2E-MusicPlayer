858ecc0ce308c09c73103c2549294dd9983ad409
Hao1229 <s0939859391@gmail.com>
2019-08-02 15:32:48 +0800

    完成中間播放清單7成

--- js/all.js
+++ js/all.js
@@ -1,14 +1,50 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-       text: '這是一段話'
-    },
-    methods: {
-        showLibrary () {
-         $('.library').toggleClass('library-active');
-       },
-       showDiscover () {
-        $('.discover').toggleClass('discover-active');
+  el: '#app',
+  data: {
+    topMusic: [
+      {
+        singer: 'HYUKOH',
+        music: 'TOMBOY',
+        src: 'img/TOMBOY.png'
       },
+      {
+        singer: 'HYUKOH',
+        music: 'LOVE YA!',
+        src: 'img/LOVE YA!.png'
+      },
+      {
+        singer: 'HYUKOH',
+        music: 'Comes and Goes',
+        src: 'img/Comes and Goes.png'
+      },
+      {
+        singer: 'HYUKOH',
+        music: 'Gondry',
+        src: 'img/Gondry.png'
+      },
+      {
+        singer: 'HYUKOH',
+        music: '위잉위잉',
+        src: 'img/위잉위잉.png'
+      }
+    ],
+    favoriteMusic:[],
+  },
+  methods: {
+    showLibrary() {
+      $('.library').toggleClass('library-active');
     },
+    showDiscover() {
+      $('.discover').toggleClass('discover-active');
+    },
+    changeIcon (item) {
+      let vm = this;
+      if(vm.favoriteMusic.indexOf(item)<0){
+        vm.favoriteMusic.push(item)
+      }else if (vm.favoriteMusic.indexOf(item)>-1){
+        let index = vm.favoriteMusic.indexOf(item)
+        vm.favoriteMusic.splice(index,1)
+      }
+    }
+  },
 })
